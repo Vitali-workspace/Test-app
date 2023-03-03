8deef0e89b218c867a21e55e10ecd2cba9c2cc23
vitali <vitali.slepnyov@gmail.com>
2023-03-03 23:52:12 +0300

    feat: добавить компонент Map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,14 @@
+import { Link, NavLink } from 'react-router-dom';
+import './Login.css';
+
+
+function Map() {
+
+  return (
+    <section className='map'>
+      <div className='map_forest'></div>
+    </section>
+  );
+}
+
+export default Map;
