a91354c596ac905b5b0e4b920d8378c7d2632693
vitali <vitali.slepnyov@gmail.com>
2023-03-10 23:58:43 +0300

    refactor: добавить импорты картинок

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,14 @@
 import { Link, NavLink } from 'react-router-dom';
 import './Login.css';
 
+import mapForest from '../../images/forest.png';
+import maptest from '../../images/map1.png';
 
 function Map() {
 
   return (
     <section className='map'>
-      <div className='map_forest'></div>
+      <div className='map_forest'>{mapForest}</div>
     </section>
   );
 }
