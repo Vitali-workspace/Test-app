b541012dc707d49c5d630a45261c8b591e9bb5cb
vitali <vitali.slepnyov@gmail.com>
2023-03-12 23:58:54 +0300

    refactor: изменить ссылки

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,10 @@ function Footer() {
   return (
     <footer className='footer'>
       <div className='footer__container'>
-        <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
         <div className='footer__nav'>
-          <p className='footer__copyright'> {new Date().getFullYear()} </p>
           <ul className='footer__list'>
-            <li><a className='footer__link' href='https://practicum.yandex.ru/'>Яндекс.Практикум</a></li>
-            <li><a className='footer__link' href='https://github.com/Vitali-workspace'>Github</a></li>
+            <li><a className='footer__link' href='#'>Практикум</a></li>
+            <li><a className='footer__link' href='#'>Github</a></li>
           </ul>
         </div>
       </div>
